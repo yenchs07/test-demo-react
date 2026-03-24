9fad29ce4aa4dacca1f97eec6184854614cf07ee
yenchs07 <yenchs1007@gmail.com>
2026-03-24 22:22:46 +0700

    outputting list

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,18 +1,39 @@
 import React from "react";
 import UserInfor from "./UserInfor";
+import { use } from "react";
 
 
 class DisplayInfor extends React.Component {
     render() {
-        const { age, name } = this.props;
+        const { listUsers } = this.props;//object
+        // const listUsers = this.props.listUsers;
+        console.log(listUsers);
         // console.log(this.props);
         return (
             //destructuring array/object
 
             //props => viet tắt properties
             <div>
+                {listUsers.map((user, index) => {
+
+                    return (
+
+                        <div key={user.id}>
+                            <div>My name's {user.name}</div>
+                            <div>My age's {user.age}</div>
+                            <hr />
+                        </div>
+                    )
+
+                })}
+                {/* <div>My name's {this.props.name}</div>
+                <div>sap chec toi noi{this.props.age}</div>
+                <hr />
                 <div>My name's {this.props.name}</div>
                 <div>sap chec toi noi{this.props.age}</div>
+                <hr />
+                <div>My name's {this.props.name}</div>
+                <div>sap chec toi noi{this.props.age}</div> */}
 
             </div>
         )
--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -6,21 +6,29 @@ import UserInfor from "./UserInfor";
 import DisplayInfor from "./DisplayInfor";
 
 class MyComponent extends React.Component {
+    state = {
+        listUsers: [
+            { id: 1, name: "Hoi Dan IT", age: "30" },
+            { id: 2, name: "Yen Trần", age: "22" },
+            { id: 3, name: "Quỳnh Đỗ", age: "20" },
+
+        ]
+    }
 
 
 
     //JSX
     render() {
         const myInfor = ["ab", "c", "c"];
+        //dry: don't rep 
         return (
             <div>
 
                 <UserInfor />
                 <br></br>
-                <br></br>
-                <DisplayInfor name="Yen Trần" age="30" />
-                <hr />
-                <DisplayInfor name="Yen Trần" age={26} myInfor={myInfor} />
+                <DisplayInfor listUsers={this.state.listUsers}
+                />
+
             </div>
 
         );
